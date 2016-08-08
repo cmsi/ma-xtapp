423c2f7c1563f9860cb39a47d289eace9f04c5a5
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2016-08-08 10:52:48 +0900

    New upstream release 160715

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ echo "DATA_DIR: $SCRIPT_DIR"
 echo "BUILD_DIR: $BUILD_DIR"
 echo "VERSION: $VERSION"
 
-if [ -f "$DATA_DIR/xTAPP-rc-$VERSION.tgz" ]; then :; else
-  echo "Error: $DATA_DIR/xTAPP-rc-$VERSION.tgz not found"
+if [ -f "$DATA_DIR/xTAPP-develop-$VERSION.tgz" ]; then :; else
+  echo "Error: $DATA_DIR/xTAPP-develop-$VERSION.tgz not found"
   exit 127
 fi
 if [ -d "$BUILD_DIR" ]; then
@@ -23,10 +23,21 @@ fi
 
 set -x
 
-tar zxf $DATA_DIR/xTAPP-rc-$VERSION.tgz
-mv xTAPP $BUILD_DIR 
+tar zxf $DATA_DIR/xTAPP-develop-$VERSION.tgz
+mv xTAPP-develop-$VERSION $BUILD_DIR 
 tar zcf xtapp_$VERSION.orig.tar.gz $BUILD_DIR
 cp -frp $SCRIPT_DIR/xtapp/debian $BUILD_DIR
 
+if [ -n "$SETUP_ONLY" ]; then
+  exit 0
+fi
+
 cd $BUILD_DIR 
-dpkg-buildpackage -us -uc
+case "$(dpkg --print-architecture)" in
+    amd64)
+        dpkg-buildpackage
+        ;;
+    i386)
+        dpkg-buildpackage -b
+        ;;
+esac

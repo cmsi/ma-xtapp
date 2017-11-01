57ec8597c12f50f29cf90e2455e8965eedb961ca
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-11-02 00:21:56 +0900

    Support for Stretch

--- xtapp-util/build.sh
+++ xtapp-util/build.sh
@@ -7,10 +7,14 @@ cd $BUILD_DIR
 case "$(dpkg --print-architecture)" in
     amd64)
         dpkg-buildpackage -us -uc
-        mv -f ../${PACKAGE}_$VERSION_BASE*.changes ../${PACKAGE}_$VERSION_BASE*.dsc ../${PACKAGE}_$VERSION_BASE*.debian.tar.* ../${PACKAGE}_$VERSION_BASE*.orig.tar.gz ../${PACKAGE}_$VERSION_BASE*.deb $TARGET_DIR
+        awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_amd64.changes > $TARGET_DIR/${PACKAGE}_${VERSION}_amd64.changes
+        mv -f ../${PACKAGE}*_${VERSION_BASE}*.buildinfo ../${PACKAGE}*_${VERSION_BASE}*.deb $TARGET_DIR
+        mv -f ../${PACKAGE}*_${VERSION_BASE}*.dsc ../${PACKAGE}*_${VERSION_BASE}*.debian.tar.* $TARGET_DIR
+        cp -f ../${PACKAGE}*_${VERSION_BASE}*.orig.tar.gz $TARGET_DIR
         ;;
     i386)
-        dpkg-buildpackage -b -us -uc
-        mv -f ../${PACKAGE}_$VERSION_BASE*.changes ../${PACKAGE}_$VERSION_BASE*.deb $TARGET_DIR
+        dpkg-buildpackage -B -us -uc
+        awk '$3!="debug" {print}' ../${PACKAGE}_${VERSION}_i386.changes > $TARGET_DIR/${PACKAGE}_${VERSION}_i386.changes
+        mv -f ../${PACKAGE}*_${VERSION_BASE}*.buildinfo ../${PACKAGE}*_$VERSION_BASE*.deb $TARGET_DIR
         ;;
 esac
